e6188247663e72ec5c75e8878c83cea9939b85c3
devdesignerprakash <devdesignerprakash@gmail.com>
2025-08-09 10:08:04 +0545

    first commit and shadcn setup done

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+
+
+import { Button } from '@shadcn/components/ui/button'
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+    <Button>Lets build food app
+      
+    </Button>
+      
+    </>
+  )
+}
+
+export default App
